b0ab0e5ba4985ca70f8cb386aadc3821524498c4
N07070 <nelson.perdriau@gmail.com>
2015-03-28 15:46:50 +0100

    Changed a few things : * Now using a dictionnary to store the translation * Added support for punctuation and special latin chars * Added support for translation * Added support for the gap between letters AND between words (tough they stack up) * Removed Herobrine

--- pymorse.py
+++ pymorse.py
@@ -22,93 +22,53 @@ A function to output the morse code to GPIO
 A function to capture morse code from GPIO
 """
 
-import RPi.GPIO as GPIO
-import re,time, os
+#import RPi.GPIO as GPIO
+import re, time, os
+# This file contains the morse dictionnairy. Please feel free to add more translations to it ! :-)
+from morse_dict import *
 
 def get_user_text():
-    user_text = raw_input("Please enter the message you would lile to broadcast. >> ")
+    user_text = raw_input("Please enter the message you would like to broadcast. >> ")
     user_text = user_text.lower()
     word_list = list(user_text)
     return word_list
 
 
 def long_pulse(pin):
-    # os.system("ogg123 audio/long.ogg")
-    GPIO.output(pin,GPIO.HIGH)
+    #os.system("ogg123 audio/long.ogg")
+    #GPIO.output(pin,GPIO.HIGH)
     print("-")
     time.sleep(1)
-    GPIO.output(pin,GPIO.LOW) 
-	
+    #GPIO.output(pin,GPIO.LOW)
+
 def short_pulse(pin):
-    # os.system("ogg123 audio/short.ogg")
-    GPIO.output(pin,GPIO.HIGH)
+    #os.system("ogg123 audio/short.ogg")
+    #GPIO.output(pin,GPIO.HIGH)
     print(".")
     time.sleep(0.5)
-    GPIO.output(pin,GPIO.LOW)
+    #GPIO.output(pin,GPIO.LOW)
+
+def short_gap():
+    print("Short gap")
+    time.sleep(1.5)
+
+def long_gap():
+    print("Long gap")
+    time.sleep(3.5)
+	
+
     
 
 
 def text_to_morse_code(alpha_text):
     morse_code = []
     for letter in alpha_text:
-        if letter == "a" or letter == "à" or letter == "â" or letter == "ä":
-            morse_code.append("01")
-        if letter == "b":
-            morse_code.append("1000")
-        if letter == "c":
-            morse_code.append("1010")
-        if letter == "d":
-            morse_code.append("100")
-        if letter == "e" or letter == "è" or letter == "é" or letter == "ê":
-            morse_code.append("0")
-        if letter == "f":
-            morse_code.append("0010")
-        if letter == "g":
-            morse_code.append("110")
-        if letter == "h":
-            morse_code.append("0000")
-        if letter == "i" or letter == "î" or letter == "ï":
-            morse_code.append("00")
-        if letter == "j":
-            morse_code.append("0111")
-        if letter == "k":
-            morse_code.append("101")
-        if letter == "l":
-            morse_code.append("0100")
-        if letter == "m":
-            morse_code.append("11")
-        if letter == "n":
-            morse_code.append("10")
-        if letter == "o":
-            morse_code.append("111")
-        if letter == "p":
-            morse_code.append("0110")
-        if letter == "q":
-            morse_code.append("1101")
-        if letter == "r":
-            morse_code.append("010")
-        if letter == "s":
-            morse_code.append("111")
-        if letter == "t":
-            morse_code.append("0")
-        if letter == "u":
-            morse_code.append("001")
-        if letter == "v":
-            morse_code.append("0001")
-        if letter == "w":
-            morse_code.append("011")
-        if letter == "x":
-            morse_code.append("1001")
-        if letter == "y":
-            morse_code.append("1011")
-        if letter == "z":
-            morse_code.append("1100")
-        if letter == ".":
-            morse_code.append("010101")
+        # For each letter, translate it to a sequence, then add a short gap
         if letter == " ":
-            morse_code.append(" ")
+            morse_code.append(morse_dict[letter])
         else:
-            pass
+            morse_code.append(morse_dict[letter])
+            morse_code.append("2")
     morse_code = ''.join(map(str, morse_code))
     morse_code = list(morse_code)
     return morse_code
@@ -116,15 +76,15 @@ def text_to_morse_code(alpha_text):
 def broadcast_code(code_to_broadcast, pin):
     
     # Set the board as BOARD
-    GPIO.setmode(GPIO.BOARD)
+    #GPIO.setmode(GPIO.BOARD)
     print("Set the board to BOARD")
     
     # Setup the n th pin to OUTPUT
-    GPIO.setup(pin, GPIO.OUT)
+    #GPIO.setup(pin, GPIO.OUT)
     print("Set the "+str(pin)+"th to OUTPUT")
     
     # Starting the broadcast
-    print("Starting Broadcast")
+    print("\n===================\nStarting Broadcast\n===================\n")
     start_broadcast = [0,1,0,1]
     
     for x in start_broadcast:
@@ -133,15 +93,21 @@ def broadcast_code(code_to_broadcast, pin):
         if x == 0:
             short_pulse(pin)
     
-    print("Broadcasting")
+    print("\n===================\nBroadcasting\n===================\n")
     for number in code_to_broadcast:
         if number == '1':
             long_pulse(pin)
-        if number == '0':
+        elif number == '0':
             short_pulse(pin)
+        # Between letters
+        elif number == '2':
+            short_gap()
+        # Between words
+        elif number == '3':
+            long_gap()
     
     #Boardcast end of transmission.
-    print("Ending Boardcast")
+    print("\n===================\nEnding Broadcast\n===================\n")
     end_broadcast = [0,0,0,1,0,1]
     
     for y in end_broadcast:
@@ -151,7 +117,7 @@ def broadcast_code(code_to_broadcast, pin):
             short_pulse(pin)
 
     
-    GPIO.cleanup()
+    #GPIO.cleanup()
     print("Cleaned up the board.")
 
 def get_code_broadcast():
